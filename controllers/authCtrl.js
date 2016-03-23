d7048ab6f769f3385affcbfea09e2ada8c87b6b0
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-03-23 18:32:39 -0500

    modularizing

---
+++ controllers/authCtrl.js
@@ -0,0 +1,35 @@
+'use strict'
+
+
+const User = require('../models/databaseUser')
+
+
+module.exports = {
+
+    registerUser (req, res) {
+        if(req.body.password === req.body.verify) {
+
+            User.findOne({email: req.body.email}, (err,user) => {
+                if(err) throw err
+
+                if(user) {
+                    res.sendStatus(403).send('there was already an email in there')
+                } else {
+                    User.create(req.body, (err, user) => {
+                        console.log('req.body in the back end', user)
+                        if(err) throw err
+
+                        res.status(200).send('created a new profile')
+                    })
+                }
+            })
+
+        } else {
+            res.status(401).send('passwords do not match!')
+        }
+    }
+
+}
+
+
+
--- routes/noteListRtr.js
+++ routes/noteListRtr.js
@@ -2,50 +2,28 @@
 
 const express = require('express')
 const router = express.Router()
+
 const databaseCtrl = require('../controllers/databaseCtrl')
-const User = require('../models/databaseUser')
+const authCtrl = require('../controllers/authCtrl')
 
 
-// router.get('/api/login', databaseCtrl.loginUser)
+
+
+router.post('/api/register', authCtrl.registerUser)
+
 router.get('/api/userdata', databaseCtrl.getUserInfo)
 
 
 
-router.post('/api/register', (req, res) => {
-    // see what you get back in terminal for req.body and do comparisons
-    // console.log('req.body', req.body)
-    if(req.body.password === req.body.verify) {
 
-        User.findOne({email: req.body.email}, (err,user) => {
-            if(err) throw err
-
-            if(user) {
-                res.sendStatus(403).send('there was already an email in there')
-            } else {
-                User.create(req.body, (err, user) => {
-                    console.log('req.body in the back end', user)
-                    if(err) throw err
-
-                    res.status(200).send('created a new profile')
-                })
-            }
-        })
-
-    } else {
-        res.status(401).send('passwords do not match!')
-    }
-})
-
-
-
-
-
-
-
-// send instead of redirect
-// res.sendStatus(200)
 
 module.exports = router
+
+
+
+
+
+
 
 
 

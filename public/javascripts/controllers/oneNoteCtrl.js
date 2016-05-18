25911a23b02812e444f8437ba0a66adc2936beff
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-05-17 19:44:56 -0500

    can edit but not save and update yet. making a new branch

--- public/javascripts/controllers/oneNoteCtrl.js
+++ public/javascripts/controllers/oneNoteCtrl.js
@@ -23,4 +23,15 @@ app.controller('oneNoteCtrl', ['$http', '$state', '$stateParams', function($http
     }
 
 
+    self.saveEntry = function() {
+        console.log('heard the save click')
+        let entryInfo = {title: self.title, entry: self.entry}
+
+        $http.post('/api/userdata', entryInfo)
+        .then((data) => {
+            self.status = data.data.status
+        })
+    }
+
+
 }])
